39317e6879a409a8939896607297c1c2a94ad669
John Thomas Shaw Valanidas <john.valanidas@gmail.com>
2024-12-30 15:34:34 -0500

    basic crud operations

---
+++ src/utils.ts
@@ -0,0 +1,15 @@
+export interface Identifiable {
+    readonly id: string;
+}
+
+export interface Resolvable<T> {
+    readonly resolved: boolean;
+    resolve(): Promise<T>;
+}
+
+
+export interface Reference<T> extends Resolvable<T | undefined> {
+    readonly value: T | undefined;
+}
+
+export type IdentifiableReference<T> = Reference<T> & Identifiable;
