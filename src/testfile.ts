7fb6ee0a2c1aac0766aef7dae56874780a840d8f
John Thomas Shaw Valanidas <john.valanidas@gmail.com>
2025-01-01 15:25:16 -0500

    add basic model

--- src/testfile.ts
+++ src/testfile.ts
@@ -1,8 +1,8 @@
 import { initializeApp, applicationDefault } from 'firebase-admin/app'
 import { firestore } from 'firebase-admin'
 import { FirebaseDataManager } from './firestoreDataManager'
-import { DocumentData, FirestoreDataConverter, QueryDocumentSnapshot, SnapshotOptions } from './firestoreAppCompatTypes'
-import firebase from 'firebase/compat/app'
+import { DocumentData, Firestore, FirestoreDataConverter, QueryDocumentSnapshot, SnapshotOptions } from './firestoreAppCompatTypes'
+import { Reference } from './reference'
 
 
 
@@ -14,11 +14,12 @@ interface Product {
 
 // TODO: setup a way for the serialization type to pick up on the
 // interface's props.
-const productConverter: FirestoreDataConverter<Product, Product> = {
-    toFirestore: function (modelObject: Product): Product {
+const productConverter: FirestoreDataConverter<Product> = {
+    toFirestore: function (modelObject: Product): Product { //NOTE: this can be any return type
         return {
             name: modelObject.name,
-            description: modelObject.desciption,
+            desciption: modelObject.desciption,
+            qty: modelObject.qty
         }
     },
     fromFirestore: function (snapshot: QueryDocumentSnapshot, options: SnapshotOptions): Product {
@@ -26,8 +27,33 @@ const productConverter: FirestoreDataConverter<Product, Product> = {
         return {
             name: data.name,
             desciption: data.desciption,
+            qty: data.qty
         }
     },
+}
+
+type UpdateProductParams = Partial<Pick<Product, 'qty' | 'desciption' | 'name'>>
+
+abstract class ProductModel {
+    abstract getProduct(id: string): Promise<Reference<Product>>;
+    abstract updateProduct(id: string, params: UpdateProductParams): Promise<void>
+}
+class FirebaseProductModel implements ProductModel {
+    private datamanager: FirebaseDataManager<Product>;
+
+    constructor(
+        private readonly db: Firestore,
+        converter: FirestoreDataConverter<Product>) {
+            this.datamanager = new FirebaseDataManager<Product>(db as any, 'temp', converter)
+    }
+
+    async getProduct(id: string): Promise<Reference<Product>> {
+        return this.datamanager.getRef(id);
+    }
+    updateProduct(id: string, params: UpdateProductParams): Promise<void> {
+        this.datamanager.update(id, {...params})
+    }
+    
 }
 
 const main = async () => {
@@ -37,10 +63,11 @@ const main = async () => {
     })
     const db = firestore()
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const productDatamanger = new FirebaseDataManager<Product>(db as any, 'temp', productConverter)
+    const productModel = new FirebaseProductModel(db as any, productConverter)
+    productModel.updateProduct("apples", {qty: 4})
     // const ref = productDatamanger.getRef("apples")
-    productDatamanger.create({ name: 'fsdfsm;', desciption: 'fasdfs' })
-    productDatamanger.update('apples', { name: 'test', desciption: 'test'})
+    // productDatamanger.create({ name: 'fsdfsm;', desciption: 'fasdfs' })
+    // productDatamanger.update('apples', { name: 'test', desciption: 'test'})
     // const product = await ref.resolve();
     // console.log(product)
 }
